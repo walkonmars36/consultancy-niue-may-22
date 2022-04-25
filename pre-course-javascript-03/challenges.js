9450233d2fc256c8a97a35dffc6bf78a80bdeaec
Mark Lawson <mark@walkonmars.co.uk>
2022-04-25 15:26:03 +0100

    challenge 2 complete

--- pre-course-javascript-03/challenges.js
+++ pre-course-javascript-03/challenges.js
@@ -9,3 +9,16 @@ const isNumberPositive = (num) => {
 
 console.log(isNumberPositive(-1)); // returns false
 console.log(isNumberPositive(10)); // returns true
+
+/* 2: Write a function that takes a number of days and converts it into an age.
+convertDaysToAge(3650); // returns 10
+convertDaysToAge(6570); // returns 18
+*/
+
+const convertDaysToAge = (num) => {
+  const age = num / 365;
+  return age;
+};
+
+console.log(convertDaysToAge(3650)); // returns 10
+console.log(convertDaysToAge(6570)); // returns 18
