e7d5940c6808eefe1bbdd759144ca0247bd61ee9
Mark Lawson <mark@walkonmars.co.uk>
2022-04-26 18:07:15 +0100

    added images

---
+++ pre-course-web-fundamentals-02/02-module-project/script.js
@@ -0,0 +1,14 @@
+const time = document.querySelector(".time");
+
+const updateClock = () => {
+  const now = new Date();
+  const hours = now.getHours();
+  let minutes = now.getMinutes();
+
+  minutes = minutes < 10 ? "0" : minutes;
+
+  time.innerHTML = hours + ":" + minutes;
+};
+
+updateClock();
+setInterval(updateClock, 1000);
