00f27252bcf3cb8e8756a0437fb038343a1e29fc
Mark Lawson <mark@walkonmars.co.uk>
2022-05-30 14:08:09 +0100

    challenge complete

--- fetch/dog.js
+++ fetch/dog.js
@@ -4,17 +4,29 @@ const getDog = document.getElementById("get-dog-button");
 
 const getFiveDogs = document.getElementById("get-five-dog-button");
 
-const getRandomDog = async () => {
-  const response = await fetch("https://dog.ceo/api/breeds/image/random");
-  const data = await response.json();
-  console.log(data);
+// ///
 
-  dogContainer.innerHTML = `
+const getRandomDog = async () => {
+  try {
+    const response = await fetch("https://dog.ceo/api/breeds/image/random");
+
+    if (!response.ok) {
+      throw new Error(response.status + " error with request");
+    }
+
+    const data = await response.json();
+    console.log(data);
+    dogContainer.innerHTML = `
       <div>
       <img src=${data.message} />
       </div>
       `;
+  } catch (error) {
+    alert(error.message);
+  }
 };
+
+// ///
 
 const getFiveRandomDogs = async () => {
   const response = await fetch("https://dog.ceo/api/breeds/image/random/5");
@@ -30,6 +42,8 @@ const getFiveRandomDogs = async () => {
   `)
   );
 };
+
+// ///
 
 getDog.addEventListener("click", getRandomDog);
 getFiveDogs.addEventListener("click", getFiveRandomDogs);
